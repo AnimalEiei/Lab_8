5c5374c8bbbb87679f0e5baf4f43f7a6084a9d80
Poomkawin <poomkawin@outlook.com>
2014-03-13 20:10:15 +0700

    paintEvent(drawPoint)

--- Lab_8/Lab08_02.py
+++ Lab_8/Lab08_02.py
@@ -3,23 +3,40 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 
 class PaintWidget(QWidget):
-    WINDOW_WIDTH = 300
-    WINDOW_HEIGHT = 300
+    WINDOW_WIDTH = 900
+    WINDOW_HEIGHT = 900
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUI()
+        self.pointList = set()
 
     def setupUI(self):
         self.setFixedSize(self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
 
-    def paintEvent(self, e):
-        painter = QPainter(self)
+    def clearAllPoint(self):
+        self.pointList.clear()
 
+    def paintEvent(self, e):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setBrush(Qt.red)
+        for i in self.pointList:
+            painter.drawPoint(i)
+        painter.end()
+        self.update()
+
+    def mouseMoveEvent(self, e):
+        super().mouseMoveEvent(e)
+        self.pointList.add(e.pos())
+        
+        
 
 class SimplePaintProgram(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUI()
+        self.clearBt.clicked.connect(self.paintArea.clearAllPoint)
+        #self.clearBt.clicked.connect(self.clear)
 
 
     def setupUI(self):
@@ -27,15 +44,15 @@ class SimplePaintProgram(QWidget):
 
         layout = QVBoxLayout()
         
-        paintArea = PaintWidget()
+        self.paintArea = PaintWidget()
         label = QLabel("Drag the mouse to draw")
         label.setAlignment(Qt.AlignHCenter)
-        clearBt = QPushButton("Clear")
-        clearBt.clicked.connect()
+        self.clearBt = QPushButton("Clear")
         
-        layout.addWidget(paintArea)
+        
+        layout.addWidget(self.paintArea)
         layout.addWidget(label)
-        layout.addWidget(clearBt)
+        layout.addWidget(self.clearBt)
 
         self.setLayout(layout)
 
