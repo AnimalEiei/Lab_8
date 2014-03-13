493b0c8bc1a4b805c465bcdf5f6fd7220a7a8b94
Poomkawin <poomkawin@outlook.com>
2014-03-13 18:38:42 +0700

    create class Disk

---
+++ Lab_8/Disk.py
@@ -0,0 +1,10 @@
+class Disk ():
+    def __init__ (self, name, pos_x, pos_y, height, width, color) :
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.height = height
+        self.width = width
+        self.color = color
+
+    def showdisk () :
